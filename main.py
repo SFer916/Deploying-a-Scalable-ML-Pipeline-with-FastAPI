1624f370ca51646a6341730664210f728f7887bc
root <root@SAF916>
2024-12-13 00:00:31 -0500

    Your commit message here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import pickle
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
--- test_ml.py
+++ test_ml.py
@@ -1,67 +1,45 @@
 import pytest
 # TODO: add necessary import
 import numpy as np
-from sklearn.metrics import precision_score, recall_score
-from ml.model import inference, compute_model_metrics, performance_on_categorical_slice
-from ml.data import process_data
-
-def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
-):
-    if label is not None:
-        y = X[label]
-        X = X.drop([label], axis=1)
-    else:
-        y = np.array([])
-
-    X_categorical = X[categorical_features].values
-    X_continuous = X.drop(*[categorical_features], axis=1)
-
-    if training is True:
-        encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
-        lb = LabelBinarizer()
-        X_categorical = encoder.fit_transform(X_categorical)
-        y = lb.fit_transform(y).ravel()
-    else:
-        X_categorical = encoder.transform(X_categorical)
-        try:
-            y = lb.transform(y.values).ravel()
-        # Catch the case where y is None because we're doing inference.
-        except AttributeError:
-            pass
-
-    X = np.concatenate([X_continuous, X_categorical], axis=1)
-    return X, y, encoder, lb
+from sklearn.metrics import precision_score, recall_score, fbeta_score
+from train_model import compute_model_metrics, process_data
 
 # TODO: implement the first test. Change the function name and input as needed
 @pytest.fixture(scope="session")
-def test_one(model):
+def test_one(y_test, preds):
     """
     # add description for the first test
-    Ensure model has 15 columns
-    """ 
-    assert model.shape == 15
+    testing if fbeta score is less than 1
+    """
+    y_test = [0,1,0,1]
+    preds = [0,1,0,1]
+    p, r, fb = compute_model_metrics(y_test, preds)
+    assert fb < 1
     pass
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two(y, preds):
+def test_two(y_test, preds):
     """
     # add description for the second test
     testing if precision is less than 1
     """
     # Your code here
-    precision = precision_score(y, preds, zero_division=1)
-    assert precision < 1
+    y_test = [0,1,0,1]
+    preds = [0,1,0,1]
+    p, r, fb = compute_model_metrics(y_test, preds)
+    assert p < 1
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three(y, preds):
+def test_three(y_test, preds):
     """
     # add description for the third test
     testing if precision is less than 1
     """
     # Your code here
-    recall = recall_score(y, preds, zero_division=1)
-    assert recall < 1
+    y_test = [0,1,0,1]
+    preds = [0,1,0,1]
+    p, r, fb = compute_model_metrics(y_test, preds)
+    assert r < 1
     pass
